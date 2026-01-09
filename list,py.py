10ae3004a05734557be4b0de4350ffe72384e77a
Ruchita Aigali <ruchitaaigaliaigali@gmail.com>
2026-01-09 18:29:12 +0530

    Add files via upload

---
+++ list,py.py
@@ -0,0 +1,14 @@
+employees=["Ruchita","Shree","Vivi"]
+print(employees)
+print(employees[0])
+print(employees[-1])
+print(employees[-3:-1])
+employees.append("Geeta")
+employees.insert(2,"Anu")
+print(employees)
+employees.sort()
+print(employees)
+employees.reverse()
+print(employees)
+len(employees)
+
