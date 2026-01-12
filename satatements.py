10ff40d0e11d1212d878524bcf69a8d149633c2d
Ruchita Aigali <ruchitaaigaliaigali@gmail.com>
2026-01-12 21:05:58 +0530

    Add files via upload

---
+++ satatements.py
@@ -0,0 +1,17 @@
+num=float(input("enter the number"))
+if num>0:
+    print("number is positive")
+else:
+    print("number is negative")
+
+
+
+
+num=int(input("enter the number"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+ 
+
+      
