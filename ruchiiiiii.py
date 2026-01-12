eb97c2b32284204d5dc69af7c5a4ad6147f7052d
Ruchita Aigali <ruchitaaigaliaigali@gmail.com>
2026-01-12 21:08:26 +0530

    Add files via upload

---
+++ ruchiiiiii.py
@@ -0,0 +1,6 @@
+username=input("enter the name")
+password=(input("enter the password"))
+if username=="ddadmin"and password=="dd@2025":
+   print("login sucessfull")
+else:
+  print("access denied")
